a0694e269af0cd9189777765c4eb500ee1431dab
iamjimah <iamjimah@gmail.com>
2021-03-20 10:03:53 -0700

    blinkz

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import CountrySelector from "./components/CountrySelector";
+import Header from "./components/Header";
+import Stats from "./components/Stats";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="App">
+        <h1>WORLD STATISTICS</h1>
+        <Stats url="https://covid19.mathdro.id/api" />
+        <CountrySelector />
+      </div>
+    </>
   );
 }
 
